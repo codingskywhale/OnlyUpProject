f44f1dd8899b72a4a29fe1b6d585edf7f108344d
gpjj <iwbgp019@naver.com>
2024-06-24 20:59:40 +0900

    feat : 엔딩씬 플레이 시간 연동

---
+++ Assets/Scripts/UI/EndingSceneUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingSceneUI : MonoBehaviour
+{
+    public TextMeshProUGUI playTimeText;
+
+    void Start()
+    {
+        playTimeText.text = "Play Time: \n" + GameManager.Instance.TimerFormat();
+    }
+}
